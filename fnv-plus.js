775d00965de4edf19db707239a7d0cdf71add5c4
tjwebb <travis@traviswebb.com>
2013-11-30 06:05:30 -0500

    improved performance

--- fnv-plus.js
+++ fnv-plus.js
@@ -10,28 +10,34 @@ var fnv = (function () {
     default_bitlen = 32,
     fnv_constants = {
       32: {
-        prime: new BigInteger('0x01000193'),
-        offset: new BigInteger('0x811C9DC5')
+        prime:  new BigInteger('01000193', 16),
+        offset: new BigInteger('811C9DC5', 16),
+        mask:   new BigInteger('FFFFFFFF', 16)
       },
       64: {
-        prime: new BigInteger('0x00000100000001B3'),
-        offset: new BigInteger('0xCBF29CE484222325')
+        prime:  new BigInteger('00000100000001B3', 16),
+        offset: new BigInteger('CBF29CE484222325', 16),
+        mask:   new BigInteger('FFFFFFFFFFFFFFFF', 16)
       },
       128: {
-        prime: new BigInteger('0x0000000001000000000000000000013B'),
-        offset: new BigInteger('0x6C62272E07BB014262B821756295C58D')
+        prime:  new BigInteger('0000000001000000000000000000013B', 16),
+        offset: new BigInteger('6C62272E07BB014262B821756295C58D', 16),
+        mask:   new BigInteger('FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF', 16)
       },
       256: {
-        prime: new BigInteger('0x0000000000000000000001000000000000000000000000000000000000000163'),
-        offset: new BigInteger('0xDD268DBCAAC550362D98C384C4E576CCC8B1536847B6BBB31023B4C8CAEE0535')
+        prime:  new BigInteger('0000000000000000000001000000000000000000000000000000000000000163', 16),
+        offset: new BigInteger('DD268DBCAAC550362D98C384C4E576CCC8B1536847B6BBB31023B4C8CAEE0535', 16),
+        mask:   new BigInteger('FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF', 16)
       },
       512: {
-        prime: new BigInteger('0x00000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000157'),
-        offset: new BigInteger('0xB86DB0B1171F4416DCA1E50F309990ACAC87D059C90000000000000000000D21E948F68A34C192F62EA79BC942DBE7CE182036415F56E34BAC982AAC4AFE9FD9')
+        prime: new  BigInteger('00000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000157', 16),
+        offset: new BigInteger('B86DB0B1171F4416DCA1E50F309990ACAC87D059C90000000000000000000D21E948F68A34C192F62EA79BC942DBE7CE182036415F56E34BAC982AAC4AFE9FD9', 16),
+        mask:   new BigInteger('FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF', 16)
       },
       1024: {
-        prime: new BigInteger('0x000000000000000000000000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000018D'),
-        offset: new BigInteger('0x0000000000000000005F7A76758ECC4D32E56D5A591028B74B29FC4223FDADA16C3BF34EDA3674DA9A21D9000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004C6D7EB6E73802734510A555F256CC005AE556BDE8CC9C6A93B21AFF4B16C71EE90B3')
+        prime: new  BigInteger('000000000000000000000000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000018D', 16),
+        offset: new BigInteger('0000000000000000005F7A76758ECC4D32E56D5A591028B74B29FC4223FDADA16C3BF34EDA3674DA9A21D9000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004C6D7EB6E73802734510A555F256CC005AE556BDE8CC9C6A93B21AFF4B16C71EE90B3', 16),
+        mask:   new BigInteger('FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF', 16)
       }
     };
 
@@ -43,10 +49,7 @@ var fnv = (function () {
         return value.toString();
       },
       hex: function () {
-        return '0x' + value.toString(16);
-      },
-      base64: function () {
-        return value.toString(64);
+        return value.toString(16);
       },
       str: function () {
         return value.toString(36);
@@ -54,28 +57,20 @@ var fnv = (function () {
     };
   }
 
-  function shift (hash, bitlen) {
-    var hashlen = new BigInteger((hash.bitLength()).toString()),
-      rshift = bitlen.max(hashlen.subtract(bitlen));
-
-    return hash.shiftRight(rshift);
-  }
-
   function hashGeneric (str, _bitlen) {
-    var bitlen = new BigInteger((_bitlen || default_bitlen).toString()),
+    var bitlen = new BigInteger((_bitlen || default_bitlen).toString(), 10),
       prime = fnv_constants[bitlen].prime,
       offset = fnv_constants[bitlen].offset,
+      mask = fnv_constants[bitlen].mask,
       hash = offset,
       trunc = Math.pow(bitlen, 2);
 
-    for (var i = 0; i < str.length; ++i) {
-      hash = hash.xor(new BigInteger(str[i]));
-      hash = hash.multiply(prime);
-      if (hash.bitLength() >= trunc) {
-        hash = shift(hash, bitlen);
-      }
+    for (var i = 0; i < str.length; i++) {
+      hash = hash.xor(new BigInteger(str.charCodeAt(i).toString(), 10))
+                 .multiply(prime)
+                 .and(mask);
     }
-    return new FnvHash(shift(hash, bitlen), bitlen);
+    return new FnvHash(hash, bitlen);
   }
 
   return {
--- spec/fnv-plus.js
+++ spec/fnv-plus.js
@@ -25,7 +25,7 @@ describe('fnv-plus', function () {
         moretext: 'lorem ipsum total random junk 23i2jnlkwjbflksdbf'
       };
     },
-    generations = [ 1, 64, 1*K, 2*K, 4*K, 8*K, 16*K, 32*K, 128*K ],
+    generations = [ 1, 1*K, 2*K, 4*K, 8*K, 16*K, 32*K, 128*K, 1*M ],
     gentime,
     ascii = { },
 
@@ -33,13 +33,12 @@ describe('fnv-plus', function () {
      * Max allowable time in ms to hash 1kb of data with 32-bit fnv. Used to 
      * compute derivative benchmarks for other payload sizes and bitlengths.
      */
-    t = 10,
+    t = 5,
 
     performant = function (g, bitlen) {
-      var r = (generations[g] / generations[0]),
-        x = Math.pow((bitlen / 32), 2) * t * r;
-      //console.log('gen: '+ g + ' bitlen: '+ bitlen +' max: '+ x);
-      return x;
+      var r = (generations[g] / generations[1]),
+        x = Math.pow((bitlen / 32), 2) * t * Math.max(r, 1);
+      return x + 10;
     };
 
   // generate a bunch of ascii data
@@ -56,15 +55,14 @@ describe('fnv-plus', function () {
         ascii[g] += JSON.stringify(data[g]);
       }
       ascii[g].slice(0, generations[g] - 1);
+      assert(ascii[g].length >= generations[g], 'test data of insufficient size');
     }
     gentime = new Date().valueOf() - t1;
   });
 
-  it('should generate test data', function () {
-    for (var g = 0; g < generations.length; g++) {
-      assert(ascii[g].length >= generations[g], 'test data of insufficient size');
-    }
-  });
+  for (var g = 0; g < generations.length; g++) {
+    it('generated '+ (generations[g] / K).toFixed(0) +'k of test data');
+  }
 
   describe('#hash()', function () {
     describe('32bit', function () {
@@ -73,11 +71,11 @@ describe('fnv-plus', function () {
             h2 = fnv.hash(hash2),
             h3 = fnv.hash(generate());
 
-        assert.equal(h1.hex(), '0xb23eba32');
-        assert.equal(h2.hex(), '0xa0714758');
+        assert.equal(h1.hex(), 'd58b3fa7');
+        assert.equal(h2.hex(), 'ef693ff0');
         assert.ok(h3.hex());
       });
-      it('should be performant', function () {
+      it('32-bit should be performant', function () {
         for (var g = 0; g < generations.length; g++) {
           var t1 = new Date().valueOf(),
             t2, max;
@@ -98,8 +96,8 @@ describe('fnv-plus', function () {
             h2 = fnv.hash(hash2, 64),
             h3 = fnv.hash(generate(), 64);
 
-        assert.equal(h1.hex(), '0xa8dd8fbdc2b13ffc');
-        assert.equal(h2.hex(), '0xc8827d2b104f80f8');
+        assert.equal(h1.hex(), '779a65e7023cd2e7');
+        assert.equal(h2.hex(), '7404cea13ff89bb0');
         assert.ok(h3.hex());
       });
       it('should be performant', function () {
@@ -123,8 +121,8 @@ describe('fnv-plus', function () {
             h2 = fnv.hash(hash2, 128),
             h3 = fnv.hash(generate(), 128);
 
-        assert.equal(h1.hex(), '0x9176e616dd2a39b6cfdc008d3c72874e');
-        assert.equal(h2.hex(), '0x99397cec4c8bc61a4723614a0f5c9dad');
+        assert.equal(h1.hex(), '6c155799fdc8eec4b91523808e7726b7');
+        assert.equal(h2.hex(), '577ea59947cc87c26ffa73dd35a3f550');
         assert.ok(h3.hex());
       });
       it('should be performant', function () {
@@ -148,12 +146,12 @@ describe('fnv-plus', function () {
             h2 = fnv.hash(hash2, 256),
             h3 = fnv.hash(generate(), 256);
 
-        assert.equal(h1.hex(), '0xe44e1bcf4297098f75a6cfed56748388180838c1ebb0341e8c35cd4d6511ee4a');
-        assert.equal(h2.hex(), '0xbd40ac089a517f6754d970f7b08174bfbed9118ce48ddd3c77f647ef04174b2e');
+        assert.equal(h1.hex(), 'ecc3cf2e0edfccd3d87f21ec0883aad4db43eead66ce09eb4a97e04e1a184527');
+        assert.equal(h2.hex(), '19395122fd2327ae0ddc5e67d8c001f4b93c6e35d422c6c62813d6cf60a77dd0');
         assert.ok(h3.hex());
       });
-      it('should be performant **256bit hash performance degrades quickly on > 32k message size**', function () {
-        for (var g = 0; g < 6; g++) {
+      it('should be performant', function () {
+        for (var g = 0; g < generations.length; g++) {
           var t1 = new Date().valueOf(),
             t2, max;
           fnv.hash(ascii[g], 256);
@@ -173,12 +171,12 @@ describe('fnv-plus', function () {
             h2 = fnv.hash(hash2, 512),
             h3 = fnv.hash(generate(), 512);
 
-        assert.equal(h1.hex(), '0x85ecd1f224bd4d123143be2910e940a8814d07413f32e085b6c39c8aa372cd197934018aeed1eab9697d228a34190e02e43306c1dd298b7453bc11919cc1a537');
-        assert.equal(h2.hex(), '0x89820672e3e4e6a97f21efcd0854a49318e0eacb7fcea60776e9e0ea7de9a4e7053c716a0c9554bed99b1274a5d0c109650f3a63d4e11d149c6f4bad6e94390b');
+        assert.equal(h1.hex(), '2b9c19ec56ccf98da0f227cc82bfaacbd8350928bd2ceacae7bc8aa13e747f5c43ca4e2e98fc25e94e4e805675545ee95a3b968c0acfaecb90aea2fdbcd4de0f');
+        assert.equal(h2.hex(), '881463aa6428ced46b62d3702311d326af7dcff79deb64d3f0a1a7eec2957f6718f2ade6cf47d266433f38e535a9760ef62c7c27184809d83b0f2b2a8d9d69d8');
         assert.ok(h3.hex());
       });
-      it('should be performant **512bit hash performance degrades quickly on > 8k message size**', function () {
-        for (var g = 0; g < 4; g++) {
+      it('should be performant', function () {
+        for (var g = 0; g < generations.length; g++) {
           var t1 = new Date().valueOf(),
             t2, max;
           fnv.hash(ascii[g], 512);
@@ -198,12 +196,12 @@ describe('fnv-plus', function () {
             h2 = fnv.hash(hash2, 1024),
             h3 = fnv.hash(generate(), 1024);
 
-        assert.equal(h1.hex(), '0xb47f6b962cd72efe569e7914ee6b160f9972c32d690d989cbda4430aaf849173e4a6b504ba7e31384d8709d150e33c28dd784e61c0747eef5f43137520529e748f727ab2f31ba53d0c0cf478a462562f5a6863fa2b57d45101f89dcd0589dc116ce452ac6adde0a200a297ac62da1c4189f116553eceff448318e30897c5b1b0');
-        assert.equal(h2.hex(), '0x8e29b958ddf0a3c522c2f2253023c79e4a8c533f5d2e7d175b0591a522f9b7382f37c287abe7d63268957e1da47885a810634b06774ac23b7e15fea63d7454e40402ca98ba34c88f53fad34192ef015f430185136419e7fb24f5262084510678087864e569e068305d2815593ec4191a01bcd157dfca4ce9c52ac6bd638b7e94');
+        assert.equal(h1.hex(), '3fa9d253e52ae80105b382c80a01e27a53d7bc1d201efb47b38f4d6e465489829d7d272127d20e1076129c00000000000000000000000000000000000000000000000000000000000000000000000000000253eb20f42a7228af9022d9f35ece5bb71e40fcd8717b80d164ab921709996e5c43aae801418e878cddf968d4616f');
+        assert.equal(h2.hex(), '3a13c61b1e04c267aa85cf164e8c9d7c5b2ab14e34c846455bc878e818913e603d5c5a3183a7a82129d420000000002c78c4ffbd2756340f2832a6ab43a50f84596efc9c7ce1ea25ed87cfae6f961834673b91c1f6cf8ee52e689d7188f061f5ac75c344e122b851daa50168b1b854ded8e8ed3bb94747c596e9a13b28ee2220');
         assert.ok(h3.hex());
       });
-      it('should be performant **1024bit hash performance degrades quickly on > 1k message size**', function () {
-        for (var g = 0; g < 2; g++) {
+      it('should be performant', function () {
+        for (var g = 0; g < generations.length; g++) {
           var t1 = new Date().valueOf(),
             t2, max;
           fnv.hash(ascii[g], 1024);
@@ -231,25 +229,25 @@ describe('fnv-plus', function () {
 
     it('#str()', function () {
       var h1 = fnv.hash(hash1);
-      assert.equal(h1.str(), '1dgfu42');
-      assert.equal(h2.str(), '18im5i0');
+      assert.equal(h1.str(), '1n91413');
+      assert.equal(h2.str(), '1ufesq8');
 
-      assert.equal(h3.str(), '2kg3e4gji835o');
-      assert.equal(h4.str(), '31rr8j2d719fc');
+      assert.equal(h3.str(), '1th7cxzlyc0dj');
+      assert.equal(h4.str(), '1rik8q0d89h9s');
     });
     it('#hex()', function () {
-      assert.equal(h1.hex(), '0xb23eba32');
-      assert.equal(h2.hex(), '0xa0714758');
+      assert.equal(h1.hex(), 'd58b3fa7');
+      assert.equal(h2.hex(), 'ef693ff0');
 
-      assert.equal(h3.hex(), '0xa8dd8fbdc2b13ffc');
-      assert.equal(h4.hex(), '0xc8827d2b104f80f8');
+      assert.equal(h3.hex(), '779a65e7023cd2e7');
+      assert.equal(h4.hex(), '7404cea13ff89bb0');
     });
     it('#dec()', function () {
-      assert.equal(h1.dec(), '2990455346');
-      assert.equal(h2.dec(), '2691778392');
+      assert.equal(h1.dec(), '3582672807');
+      assert.equal(h2.dec(), '4016652272');
 
-      assert.equal(h3.dec(), '12168039813402935292');
-      assert.equal(h4.dec(), '14448248178468684024');
+      assert.equal(h3.dec(), '8618312879776256743');
+      assert.equal(h4.dec(), '8360034000264797104');
     });
   });
 });
